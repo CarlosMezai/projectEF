c48906ed127eb40af3e91774657ccafcb8382aed
CarlosMezai <mezaca85@gmail.com>
2024-02-19 13:35:23 -0500

    conexion in memory BBDD

--- Models/Tareas.cs
+++ Models/Tareas.cs
@@ -1,10 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Net.Http.Headers;
 
-namespace projectEF.Models
+namespace projectEF.Models;
 
 public class Tarea
 {
+    [Key]
     public Guid TareaId {get; set;}
+
+    [ForeignKey("CategoriaId")]
     public Guid CategoriaId {get; set;}
+
+    [Required]
+    [MaxLength(200)]
+    public string Titulo {get; set;}
+    public Prioridad PrioridadTarea {get; set;}
+
+    public DateTime FechaCreacion {get; set;}
+
+    public virtual Categoria categoria {get; set;}
+
+    [NotMapped]
+    public string Resumen {get; set;}
+
+
+}
+
+public enum  Prioridad
+{
+    Baja,
+    Media,
+    Alta
 }
