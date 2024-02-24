e859b1722d8c3d3510ffef04440285734a642c7c
CarlosMezai <mezaca85@gmail.com>
2024-02-24 14:05:06 -0500

    Conexion BBDD with Postgres

--- Program.cs
+++ Program.cs
@@ -4,16 +4,19 @@ using projectEF;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TareasContext>(x => x.UseInMemoryDatabase("TareasDB"));
-builder.Services.Add
+//builder.Services.AddDbContext<TareasContext>(x => x.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddNpgsql<TareasContext>(builder.Configuration.GetConnectionString("TareasDb"));
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+//EndPoint de conexión
 app.MapGet("/dbconexion", async ([FromServices] TareasContext  dbContext) =>
 {
     dbContext.Database.EnsureCreated();
-    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+    //return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+    return Results.Ok("Base de datos en POSTGRES: " + dbContext.Database.IsNpgsql());
 });
 
 app.Run();
