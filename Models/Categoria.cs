e0ecad97e4c367c9f0a9fc0a1b78150422325494
CarlosMezai <mezaca85@gmail.com>
2024-03-12 23:08:00 -0500

    using Fluent API for create table

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -6,11 +6,11 @@ namespace projectEF.Models;
 public class Categoria
 {
 
-    [Key]
+    //[Key]
     public Guid CategoriaId {get; set;}
 
-    [Required]
-    [MaxLength(150)]
+    //[Required]
+    //[MaxLength(150)]
     public string Nombre {get; set;}
     public string Descripcion {get; set;}
 
